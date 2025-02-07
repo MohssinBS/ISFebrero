edcb6259679d73f97df53019f4036687a8463b77
MohssinBS <bassatmohssin@gmail.com>
2025-02-07 14:20:09 +0100

    estructura inicial

---
+++ alumno/alumno.h
@@ -0,0 +1,20 @@
+#ifndef ALUMNO_H
+#define ALUMNO_H
+
+#include <string>
+#include <list>
+
+class Alumno{
+    private:
+        std::string nombrecompleto;
+        std::string dni;
+        std::string correo;
+        std::string grado;
+        std::string uni;
+        std::list<std::string> planesinscritos;
+    public:
+        Alumno(const std::string& nombrecompleto,const std::string& dni,const std::string& correo,const std::string& grado,const std::string& uni);
+
+};
+
+#endif
